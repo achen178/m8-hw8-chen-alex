5ab3cc97aa870cf5fca81019f1b72daf18327fc2
Alex Chen <32882075+achen178@users.noreply.github.com>
2019-07-12 12:08:59 -0400

    Add js capability following tutorial

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 // Global Variables
-
+const myBoxes = document.getElementsByClassName('block');
 
 // The Functionality
+function loopThrough() {
+  const changeColor = () => this.classList.toggle('new-color')
+  for (let i = 0; i < this.textContent.length * 2; i++){
+    setTimeout(changeColor, i * 300);
+  };
 
+};
 
 // The Event Listener
+for (let i = 0; i < myBoxes.length; i++){
+  myBoxes[i].addEventListener('click', loopThrough);
+};
